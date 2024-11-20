cffa1b96d7e3b3a54e5f336cc676db4a9465bca9
Kimberley Antao <kimberley2609@gmail.com>
2024-11-20 10:33:11 +0000

    File that contains main method added

---
+++ src/FirstChallenge/Main.java
@@ -0,0 +1,106 @@
+package FirstChallenge;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+/*
+* Van takes 3 compact spots
+* print when certain spots are filled
+* print how many vans are present
+* */
+
+
+public class Main {
+    private ParkingLot park1;
+
+    public Main(){
+        this.park1 = new ParkingLot();
+        createParkingLot();
+    }
+
+    public void createParkingLot(){
+        for(String[] details: ParkingSpotDetails.getSpotDetails()){
+            park1.addParkingSpot(new ParkingSpot(Integer.parseInt(details[0]), details[1]));
+        }
+    }
+
+    public void welcomeMessage(int totalSpots){
+        System.out.println("-------------------------------------------");
+        System.out.println("     Welcome to Kimberley's Parking Lot");
+        System.out.println("-------------------------------------------");
+        System.out.printf("In total there are %d spots \n", totalSpots);
+    }
+
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+        int userInput, vehicleType, spotNumber, filledSpots;
+        String vehicleNumber, vehicleSize;
+
+        String[] vehicleTypeList = {"Motorcycle", "Car", "Van"};
+        int totalSpots = park1.getNumberOfParkingSpots();
+
+        welcomeMessage(totalSpots);
+
+        while (true){
+            filledSpots = park1.getNumberOfFilledSpots();
+
+            if(totalSpots == filledSpots){
+                System.out.println("Parking lot is full");
+            }
+            else if(filledSpots == 0){
+                System.out.println("Parking lot is empty.");
+            }else{
+                System.out.printf("There are %d slots remaining.\n", (totalSpots - filledSpots));
+            }
+            System.out.println("\n--MENU--");
+            System.out.println("1. Park a vehicle \n" +
+                                "2. Remove a vehicle \n" +
+                                "3. Exit");
+            userInput = scanner.nextInt();
+            scanner.nextLine();
+            switch(userInput){
+                case 1:
+                    System.out.println("Enter the vehicle Number");
+                    vehicleNumber = scanner.nextLine();
+
+                    System.out.println("Choose the type of vehicle");
+                    System.out.println("1. Motorcycle \n2. Car \n3. Van");
+                    vehicleType = scanner.nextInt();
+
+//                    if(!vehicleType.equalsIgnoreCase("motorcycle")||!vehicleType.equalsIgnoreCase( "car")|| !vehicleType.equalsIgnoreCase("van"))
+//                    System.out.println("Enter the size");
+//                    vehicleSize = scanner.nextLine();
+
+                    park1.parkVehicle(new Vehicle(vehicleNumber, vehicleTypeList[vehicleType-1]));
+
+                    break;
+
+                case 2:
+                    System.out.println("Enter the spot number");
+                    spotNumber = scanner.nextInt();
+
+                    park1.removeVehicle(spotNumber);
+                    break;
+
+                case 3:
+                    System.out.println("Exiting the system");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid option");
+
+            }
+
+        }
+    }
+
+
+
+    public static void main(String args[]){
+
+        Main parking = new Main();
+        parking.run();
+    }
+
+}
